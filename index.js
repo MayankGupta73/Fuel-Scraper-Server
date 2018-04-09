4d5bd50cd9562fd1f0a8c428051713eec062663b
MayankGupta73 <m.mayank@hotmail.com>
2018-04-09 23:17:49 +0530

    Add db support, complete scraping and add scheduling

--- index.js
+++ index.js
@@ -1,16 +1,49 @@
-const express = require('express')
-const app = express()
-var path = require('path')
+const express = require('express');
+const app = express();
+var path = require('path');
+var mongoose = require('mongoose');
+var CronJob = require('cron').CronJob;
+
+var appdb = require('./lib/db');
 
 app.get('/', function(req, res){ res.send('Hello World!') })
-app.use('/public', express.static(path.join(__dirname, 'public')))
+//app.use('/public', express.static(path.join(__dirname, 'public')))
 
-var scraper = require('./public/scraper');
+var scraper = require('./lib/scraper');
 
-app.get('/petrol', function (req, res) {
-    var petrolPrices = scraper.scrapePrices(res);
+var mongoDB = 'mongodb://localhost/test';
+mongoose.connect(mongoDB);
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+app.get('/start', function (req, res) {
+    var petrolPrices = scraper.scrapePrices();
     //console.log(petrolPrices)
     //res.send(petrolPrices);
 })
+
+
+app.get('/fetch/:stateName', function (req, res) {
+    var prices = appdb.fetchStatePrice(req.params.stateName.toLowerCase(), res);
+})
+
+var job = new CronJob({
+    cronTime: '00 12 23 * * *',
+    onTick: function() {
+        /*
+         * Runs everyday 01:00:00 AM.
+         */
+        console.log("Scheduler starting");
+
+        scraper.scrapePrices();
+    },
+    start: false,
+    timeZone: 'Asia/Kolkata'
+});
+
+console.log('job status', job.running);
+if(!job.running)
+    job.start();
+console.log('job status', job.running);
 
 app.listen(3000, function(){ console.log('Example app listening on port 3000!') })
